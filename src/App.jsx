1a4d1dbcfe8a026c6442e57abfd8681a5105bba6
Nancy Quinonez <nancyq29@hotmail.com>
2025-07-14 18:16:27 -0500

    edited routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AllPostsPage from "./pages/AllPostsPage";
 import { useEffect, useState } from 'react';
 import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
